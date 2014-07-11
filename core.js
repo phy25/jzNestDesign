222b63dfeac0016b4f06d1c3ace6d7505b60d1e8
Phy <git@phy25.com>
2014-07-12 01:59:35 +0800

    完成发布框初步设计，和 box 的框架设计。

---
+++ core.js
@@ -0,0 +1,68 @@
+$(function(){
+	function supportsTransitions() {
+	    var b = document.body || document.documentElement,
+	        s = b.style,
+	        p = 'transition';
+
+	    if (typeof s[p] == 'string') { return true; }
+
+	    // Tests for vendor specific prop
+	    var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
+	    p = p.charAt(0).toUpperCase() + p.substr(1);
+
+	    for (var i=0; i<v.length; i++) {
+	        if (typeof s[v[i] + p] == 'string') { return true; }
+	    }
+
+	    return false;
+	}
+
+	$('#new-weibo-cover').click(function(){
+		$('#new-weibo-outer').addClass('on');
+		$('#status').focus();
+	});
+	$('#new-weibo-close').click(function(){
+		$('#new-weibo-outer').removeClass('on');
+	});
+	$('#new-weibo-content').on('keydown', function(e){
+		if(e.keyCode == 27){
+			$('#new-weibo-close').click();
+			$('#new-weibo-cover').focus();
+			e.preventDefault();
+			return false;
+		}
+		if(e.keyCode == 13 && e.ctrlKey == true){
+			$('#new-weibo-form').submit();
+			e.preventDefault();
+			return false;
+		}
+	});
+	$('#new-weibo-close').keyup(function(e){
+		if(e.keyCode == 13){
+			$('#new-weibo-cover').focus();
+		}
+	});
+	$('#new-weibo-cover').keydown(function(e){
+		if(e.keyCode == 13){
+			$(this).click();
+			e.preventDefault();
+			return false;
+		}
+	});
+	if(!supportsTransitions()){
+		$('#box-flipper-backer').hide();
+		$('#new-weibo-cover').click(function(){
+			$('#box-flipper-fronter').hide();
+			$('#box-flipper-backer').show();
+		});
+		$('#new-weibo-close').click(function(){
+			$('#box-flipper-backer').hide();
+			$('#box-flipper-fronter').show();
+		});
+	}
+	$('#new-weibo-form').submit(function(e){
+		e.preventDefault();
+		alert('Ajax here!');
+		return false;
+	});
+});
