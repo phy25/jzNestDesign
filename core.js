23186a4244f1bee70433e026ce26dd005d5dab20
Phy <git@phy25.com>
2014-07-12 13:34:57 +0800

    完成选择图片逻辑，微博显示卡片基本版完成。

--- core.js
+++ core.js
@@ -16,10 +16,64 @@ $(function(){
 
 	    return false;
 	}
+	var wbGetLength = (function(){ 
+		var trim = function(h) { 
+		try { 
+		return h.replace(/^\s+|\s+$/g, "") 
+		} catch(j) { 
+		return h 
+		} 
+		} 
+		var byteLength = function(b) { 
+		if (typeof b == "undefined") { 
+		return 0 
+		} 
+		var a = b.match(/[^\x00-\x80]/g); 
+		return (b.length + (!a ? 0 : a.length)) 
+		}; 
+
+		return function(q, g) { 
+		g = g || {}; 
+		g.max = g.max || 140; 
+		g.min = g.min || 41; 
+		g.surl = g.surl || 20; 
+		var p = trim(q).length; 
+		if (p > 0) { 
+		var j = g.min, 
+		s = g.max, 
+		b = g.surl, 
+		n = q; 
+		var r = q.match(/(http|https):\/\/[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+([-A-Z0-9a-z\$\.\+\!\_\*\(\)\/\,\:;@&=\?~#%]*)*/gi) || []; 
+		var h = 0; 
+		for (var m = 0, 
+		p = r.length; m < p; m++) { 
+		var o = byteLength(r[m]); 
+		if (/^(http:\/\/t.cn)/.test(r[m])) { 
+		continue 
+		} else { 
+		if (/^(http:\/\/)+(weibo.com|weibo.cn)/.test(r[m])) { 
+		h += o <= j ? o: (o <= s ? b: (o - s + b)) 
+		} else { 
+		h += o <= s ? b: (o - s + b) 
+		} 
+		} 
+		n = n.replace(r[m], "") 
+		} 
+		return Math.ceil((h + byteLength(n)) / 2) 
+		} else { 
+		return 0 
+		} 
+		} 
+	})();
 
 	$('#new-weibo-cover').click(function(){
-		$('#new-weibo-outer').addClass('on');
-		$('#status').focus();
+		if(!$(this).is('.active')){
+			setTimeout(function(){
+				$('#new-weibo-cover').removeClass('progress-bar-striped success error');
+			}, 600);
+			$('#new-weibo-outer').addClass('on');
+			$('#status').focus();
+		}
 	});
 	/*
 	$('#new-weibo-cover').focus(function(){
@@ -44,6 +98,31 @@ $(function(){
 			return false;
 		}
 	});
+	$('#status').on('keyup keydown paste', function(){
+		var l = 130 - wbGetLength(this.value);
+		$('#new-weibo-counter').text(l)[l < 0 ? 'addClass':'removeClass']('warning');
+	}).trigger('paste');
+
+	var onPicChange = function(e){
+		var ele = $('#pic')[0], filename = (ele.files && ele.files[0]) ? ele.files[0].name : ele.value;
+		console.log(filename);
+		if(filename){
+			$('#new-weibo-upload-btn')
+				.addClass('selected')
+				.attr('title', '已选取图片文件 ' + filename);
+		}else{
+			$('#new-weibo-upload-btn')
+				.removeClass('selected')
+				.attr('title', '上传图片文件');
+		}
+	};
+	$('#pic').on('change', onPicChange);
+	$('#new-weibo-form').on('reset', function(){ setTimeout(onPicChange, 0);});
+	$('#new-weibo-upload-btn').click(function(){
+		$('#pic').click();
+		return false;
+	});
+
 	$('#new-weibo-close').keyup(function(e){
 		if(e.keyCode == 13){
 			$('#new-weibo-cover').focus();
@@ -62,8 +141,10 @@ $(function(){
 	if(!supportsTransitions()){
 		$('#box-flipper-backer').hide();
 		$('#new-weibo-cover').click(function(){
-			$('#box-flipper-fronter').hide();
-			$('#box-flipper-backer').show();
+			if(!$(this).is('.active')){
+				$('#box-flipper-fronter').hide();
+				$('#box-flipper-backer').show();
+			}
 		});
 		$('#new-weibo-close').click(function(){
 			$('#box-flipper-backer').hide();
@@ -72,7 +153,30 @@ $(function(){
 	}
 	$('#new-weibo-form').submit(function(e){
 		e.preventDefault();
-		alert('Ajax here!');
+		// Ajax here!
+		$('#new-weibo-progress').text('正在发布');
+		$('#new-weibo-cover').addClass('progress-bar-striped active');
+		$('#new-weibo-close').click();
+		$('#status').blur();
+		setTimeout(function(){
+			$('#new-weibo-cover').removeClass('progress-bar-striped active');
+			if(Math.random() > 0.4){ // success
+				$('#new-weibo-cover').addClass('success');
+				$('#new-weibo-progress').text('发布成功');
+				// **TODO**:插入到主 timeline
+				setTimeout(function(){
+					$('#new-weibo-cover').removeClass('success');
+				}, 5000);// **TODO**:Timeout 清除
+				$('#new-weibo-form')[0].reset();
+			}else{
+				$('#new-weibo-cover').addClass('error').focus();
+				$('#new-weibo-progress').text('发布失败（网络错误）');
+				/*setTimeout(function(){
+					$('#new-weibo-cover').removeClass('progress-bar-striped error');
+				}, 5000);*/
+			}
+			
+		}, 2000);
 		return false;
 	});
 });
